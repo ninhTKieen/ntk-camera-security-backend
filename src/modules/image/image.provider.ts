17f1694f8113fc2f4639ed1e39813bf73bccd112
ninhTKieen <ninhtrungkien2811@gmail.com>
2024-07-17 14:49:06 +0700

    chore: update cloudinary provide name

--- src/modules/image/image.provider.ts
+++ src/modules/image/image.provider.ts
@@ -2,9 +2,10 @@ import { ConfigService } from '@nestjs/config';
 import { v2 as cloudinary } from 'cloudinary';
 import { TConfigs } from 'src/configs';
 import { TAppConfig } from 'src/configs/app.config';
+import { CLOUDINARY_PROVIDER_NAME } from 'src/configs/constant.config';
 
 export const ImageProvider = {
-  provide: 'CLOUDINARY',
+  provide: CLOUDINARY_PROVIDER_NAME,
   useFactory: async (configService: ConfigService<TConfigs>) => {
     return cloudinary.config({
       cloud_name:
